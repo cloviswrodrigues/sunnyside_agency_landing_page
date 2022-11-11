bd4b5a41617b67682430e2e392daf5df5b59b864
Clóvis Rodrigues <cloviswrodrigues@gmail.com>
2022-11-11 13:27:35 -0300

    created js logic close menu

--- frontend/scripts/main.js
+++ frontend/scripts/main.js
@@ -5,4 +5,17 @@ function toggleMenu(e) {
     menu.classList.toggle('activate');
 }
 
+function closeMenu() {
+    menu.classList.remove('activate');
+}
+
 hamburguer.addEventListener('click', toggleMenu)
+
+window.addEventListener('mouseup', (e) => {
+    let boxMenu = document.querySelector('.js-menu');
+    console.log(boxMenu);
+    console.log(e.target);
+    if (e.target != boxMenu) {
+        closeMenu();
+    }   
+})
